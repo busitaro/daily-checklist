a80186280153a62d18184e2c35d3125b3f70fcca
busitaro <busitaro10@gmail.com>
2022-08-01 22:25:02 +0900

    :wrench: construct infrastructure

---
+++ front/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+}
---
+++ front/src/components/organisms/MainHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <!--
+  <v-app-bar flat app dark color="parimary">
+    <div>unko</div>
+    <v-spacer />
+    <div>unko2</div>
+  </v-app-bar>
+-->
+  <v-app-bar color="primary" dark app>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>Vuetify</v-toolbar-title>
+    <v-text-field value="unkooo" />
+    <unko value="test" />
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
